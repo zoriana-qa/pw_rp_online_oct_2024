939db3652d4808a38605f497f0f07ab269e6d7fa
zoriana-qa <zorilopadchak@gmail.com>
2024-11-12 17:12:14 +0200

    1

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -6,7 +6,7 @@ test("tests Login as admin", async ({ page }) => {
     .locator("div.p-0 > div:nth-of-type(1) input")
     .fill("xotabu4@gmail.com");
   await page.locator("div.p-0 > div:nth-of-type(1) input").click();
-  await page.locator("main div:nth-of-type(2) input").fill("xotabu4@gmailz.com");
+  await page.locator("main div:nth-of-type(2) input").fill("xotassdbu4@gmailz.com");
   await page.locator("main button.custom-btn-primary").click();
   await expect(page.getByRole("heading", { name: "Login" })).not.toBeVisible();
 });
